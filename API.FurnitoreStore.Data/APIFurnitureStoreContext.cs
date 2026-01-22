8ed0f210ad83e7b5c9ec57e3ca79a01b44761cc9
MelquiadesXIII <lian.carlos.365@gmail.com>
2026-01-22 13:17:17 -0500

    Initial commit

---
+++ API.FurnitoreStore.Data/APIFurnitureStoreContext.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.FurnitoreStore.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.FurnitoreStore.Data
+{
+    public class APIFurnitureStoreContext : DbContext
+    {
+        public APIFurnitureStoreContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Client> Clients { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite();
+        }
+     }
+}
