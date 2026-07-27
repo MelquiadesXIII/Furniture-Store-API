dcd8d6046ee29e9e068504535622557601582386
Lian Carlos Gené Galvez <lian.carlos.365@gmail.com>
2026-07-27 10:07:00 -0400

    feat: Agregado la funcionalidad del Products Controller

---
+++ API.FurnitoreStore.API/Controllers/ProductsController.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.FurnitoreStore.Data;
+using API.FurnitoreStore.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.FurnitoreStore.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly APIFurnitureStoreContext _context;
+
+         public ProductsController(APIFurnitureStoreContext context)
+         {
+            _context = context;
+         }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDetails(int id)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpGet("GetByCategory/{productCategoryId}")]
+        public async Task<IEnumerable<Product>> GetByCategory(int productCategoryId)
+        {
+            return await _context.Products
+                        .Where(p => p.ProductCategoryId == productCategoryId)
+                        .ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Product product)
+        {
+            
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("Post", product.Id, product);
+        }
+
+         [HttpPut]
+         public async Task<IActionResult> Put(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Product product)
+        {
+            if (product == null) return NotFound();
+
+            _context.Products.Remove(product);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
