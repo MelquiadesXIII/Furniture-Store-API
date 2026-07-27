bb137f7ad572f23c9f750f51b330fca8741205d7
Lian Carlos Gené Galvez <lian.carlos.365@gmail.com>
2026-07-27 10:05:03 -0400

    feat: Agregado el mapeo y enrutamiento de controladores

--- API.FurnitoreStore.API/Program.cs
+++ API.FurnitoreStore.API/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -21,6 +22,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseRouting();
+app.MapControllers();
 
 var summaries = new[]
 {
