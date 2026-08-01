3bff3fdc0feab7b46908a164180aefb51347487a
Lian Carlos Gené Galvez <lian.carlos.365@gmail.com>
2026-07-31 22:36:35 -0400

    feat: Agrega la configuracion del JWT

--- API.FurnitoreStore.API/Program.cs
+++ API.FurnitoreStore.API/Program.cs
@@ -1,5 +1,9 @@
+using System.Text;
+using API.FurnitoreStore.API.Configuration;
 using API.FurnitoreStore.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +16,33 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<APIFurnitureStoreContext>(options => 
     options.UseNpgsql(builder.Configuration.GetConnectionString("APIFurnitoreStoreContext")));
 
+
+//El codigo a agregar para configuear el jwt
+builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig"));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(jwt =>
+{
+    //Aqui se guarda el valor del secret jwt
+    var key = Encoding.ASCII.GetBytes(builder.Configuration.GetSection("JwtConfig:Secret").Value!);
+
+    jwt.SaveToken = true;
+    jwt.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false, //Esto en produccion debe ser verdadero, esto valida quien emitio el token para asegurarse q no hubo nadie intermedio que cambiara el token
+        ValidateAudience = false, //Esto en produccion debe ser verdadero, que el destinatario de este token debe ser el mismo que lo esta recibiendo
+        RequireExpirationTime = false, //Debe estar en verdadero cuando se ponga el Refresh token
+        ValidateLifetime = true
+    };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +53,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseRouting();
 app.MapControllers();
 
