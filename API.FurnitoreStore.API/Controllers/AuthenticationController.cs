db3fb84c8c5ca58ffba57de773b1afc01c3e451d
Lian Carlos Gené Galvez <lian.carlos.365@gmail.com>
2026-09-01 08:49:16 -0400

    refactor: Indentacion mejorada en AuthenticationController

--- API.FurnitoreStore.API/Controllers/AuthenticationController.cs
+++ API.FurnitoreStore.API/Controllers/AuthenticationController.cs
@@ -22,8 +22,10 @@ namespace API.FurnitoreStore.API.Controllers
         private readonly UserManager<IdentityUser> _userManager;
         private readonly JwtConfig _jwtConfig;
 
-        public AuthenticationController(UserManager<IdentityUser> userManager,
-                                        IOptions<JwtConfig> jwtConfig)
+        public AuthenticationController(
+            UserManager<IdentityUser> userManager,
+            IOptions<JwtConfig> jwtConfig
+        )
         {
             _userManager = userManager;
             _jwtConfig = jwtConfig.Value;
@@ -32,50 +34,42 @@ namespace API.FurnitoreStore.API.Controllers
         [HttpPost("Register")]
         public async Task<IActionResult> Register([FromBody] UserRegistrationRequestDto request)
         {
-            if(!ModelState.IsValid) return BadRequest();
+            if (!ModelState.IsValid)
+                return BadRequest();
 
             // Verifica si el email existe
             var emailExists = await _userManager.FindByEmailAsync(request.EmailAddress);
 
-            if(emailExists != null) 
-                return BadRequest(new AuthResult()
-                {
-                    Result = false,
-                    Errors = new List<String>()
+            if (emailExists != null)
+                return BadRequest(
+                    new AuthResult()
                     {
-                        "Email already exists"
+                        Result = false,
+                        Errors = new List<String>() { "Email already exists" },
                     }
-                });
+                );
 
             // Crear usuario
             var user = new IdentityUser()
             {
                 Email = request.EmailAddress,
-                UserName = request.EmailAddress
+                UserName = request.EmailAddress,
             };
 
             var isCreated = await _userManager.CreateAsync(user);
 
-            if(isCreated.Succeeded)
+            if (isCreated.Succeeded)
             {
                 var token = GenerateToken(user);
-                return Ok(new AuthResult()
-                {
-                    Result = true,
-                    Token = token
-                });
+                return Ok(new AuthResult() { Result = true, Token = token });
             }
             else
             {
                 var errors = new List<string>();
-                foreach(var err in isCreated.Errors)
+                foreach (var err in isCreated.Errors)
                     errors.Add(err.Description);
 
-                return BadRequest(new AuthResult
-                {
-                    Result = false,
-                    Errors = errors
-                });
+                return BadRequest(new AuthResult { Result = false, Errors = errors });
             }
         }
 
@@ -87,20 +81,33 @@ namespace API.FurnitoreStore.API.Controllers
 
             var tokenDescriptor = new SecurityTokenDescriptor()
             {
-                Subject = new ClaimsIdentity(new ClaimsIdentity(new[]
-                {
-                    new Claim("Id", user.Id),
-                    new Claim(JwtRegisteredClaimNames.Sub, user.Email!),
-                    new Claim(JwtRegisteredClaimNames.Email, user.Email!),
-                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), //  JWT ID, se usa para prevenir ataques de volver a utilizar el token, se agrega un identificador unico
-                    new Claim(JwtRegisteredClaimNames.Iat, DateTime.Now.ToUniversalTime().ToString()) // Iat identifica la hora y el dia a la que fue emitida el token
-                })),
+                Subject = new ClaimsIdentity(
+                    new ClaimsIdentity(
+                        new[]
+                        {
+                            new Claim("Id", user.Id),
+                            new Claim(JwtRegisteredClaimNames.Sub, user.Email!),
+                            new Claim(JwtRegisteredClaimNames.Email, user.Email!),
+                            //  JWT ID, se usa para prevenir ataques de volver a utilizar el token, se agrega un identificador unico
+                            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                            // Iat identifica la hora y el dia a la que fue emitida el token
+                            new Claim(
+                                JwtRegisteredClaimNames.Iat,
+                                DateTime.Now.ToUniversalTime().ToString()
+                            ),
+                        }
+                    )
+                ),
                 Expires = DateTime.UtcNow.AddHours(1),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256
+                ),
             };
 
             var token = jwtTokenHandler.CreateToken(tokenDescriptor);
 
             return jwtTokenHandler.WriteToken(token);
         }
-}   }
+    }
+}
